f959a4dad4d3f1aea1f21838e0a848aa60e03ef7
McGowanJ <113394897+McGowanJericho@users.noreply.github.com>
2022-09-21 01:36:35 -0400

    Add files via upload

---
+++ Audiobooks/Audiobooks/Program.cs
@@ -0,0 +1,42 @@
+﻿bool A;
+bool B;
+bool C;
+
+string pack;
+
+string books;
+double bookBill;
+double packBill;
+double totalBill;
+
+Console.WriteLine("Which Package would you like?\n");
+Console.WriteLine("\t * Package A: For $9.95 a per month, 10 audiobooks are provided. Additional books are $2.00 per book.");
+Console.WriteLine("\t * Package B: For $13.95 per month, 20 audiobooks are provided. Additional books are $1.00 per book");
+Console.WriteLine("\t * Package C: For $19.95 per month, unlimited audiobook access is provided.");
+
+pack = Console.ReadLine();
+
+switch (pack)
+{
+    case "A":
+
+        Console.WriteLine("You have selected Package A. How many audiobooks would you like?");
+        books = Console.ReadLine();
+        books = int.Parse.(books);
+        if (books > 10)
+        {
+            bookBill = (books-10) * 2.00;
+        }
+
+
+        
+        break;
+
+    case "B":
+
+        break;
+
+    case "C":
+
+        break;
+}
